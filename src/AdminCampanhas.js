4443853cbf78141bdf12aa9128b3a5fe3fe02ae6
= <=>
2018-06-29 20:24:47 -0400

    ajustando estilo e salvando novas campanhas e removendo ok

--- src/AdminCampanhas.js
+++ src/AdminCampanhas.js
@@ -18,28 +18,48 @@ class AdminCampanhas extends Component {
         })
     }
 
-    removeCampanha = key => {
-       base.remove('campanhas/'+key, err => {
-           console.log(err)
-       })
+    handleRemoveCampanha = key => {
+        base.remove('campanhas/' + key, err => {
+            console.log(err)
+        })
     }
 
-    renderCampanha = (key, campanha) => {
+    handleRenderCampanha = (key, campanha) => {
         return (
             <li key={key}>
                 {campanha.nome}
                 &nbsp;
-            <button onClick={() => 1}>Editar</button>
-                <button onClick={() => this.removeCampanha(key)}>Remover</button>
+            <button className="btn btn-success" onClick={() => 1}>Editar</button>
+                <button className="btn btn-danger" onClick={() => this.handleRemoveCampanha(key)}>Remover</button>
             </li>)
     }
+
+    handleSave = () => {
+        const nome = this.nome.value
+        const descricao = this.descricao.value
+
+        base.push('campanhas', {
+            data: { nome, descricao },
+            then: err => {
+                if (!err) {
+                    this.nome.value = ''
+                    this.descricao.value = ''
+                }
+            }
+        })
+    }
+
 
     render() {
         return (
             <div>
                 <h1>Campanhas</h1>
+                <h2>Nova Campanha</h2>
+                Campanha: <input className="form-control" type="text" ref={ref => this.nome = ref} /> <br />
+                Descrição: <textarea className="form-control" type="text" ref={ref => this.descricao = ref} /> <br />
+                <button className="btn btn-primary" onClick={this.handleSave}>Salvar nova campanha</button>
                 <ul>
-                    {Object.keys(this.state.campanhas).map(key => this.renderCampanha(key, this.state.campanhas[key]))}
+                    {Object.keys(this.state.campanhas).map(key => this.handleRenderCampanha(key, this.state.campanhas[key]))}
 
                 </ul>
             </div>
